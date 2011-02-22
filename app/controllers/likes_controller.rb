9db55f5a428d8f63cc4a55800125cb3c94678bfc
Uiri <uiri00@gmail.com>
2011-02-22 00:44:49 -0500

    managed to find a better filter for strangers trying to like a post they can't see. Still working on that whole double like thing though.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -11,38 +11,40 @@ class LikesController < CommentsController
 
   def create
     target = current_user.find_visible_post_by_id params[:post_id]
-#    if 
-#      render :nothing => true, :status => 409
-#    end
-    text = 'likes this'
+    if !target
+      render :nothing => true, :status => 406
+    elsif target.likes.where(:person_id => current_user.id).any?
+      render :nothing => true, :status => 409
+    else
+      text = 'likes this'
 
-    if target
-      @like = current_user.build_like(text, :on => target)
+      if target
+        @like = current_user.build_like(text, :on => target)
       
-      if @like.save
-        Rails.logger.info("event=create type=like user=#{current_user.diaspora_handle} status=success like=#{@like.id}")
+        if @like.save
+          Rails.logger.info("event=create type=like user=#{current_user.diaspora_handle} status=success like=#{@like.id}")
 
-        current_user.dispatch_like(@like)
+          current_user.dispatch_like(@like)
 
-        respond_to do |format|
-          format.js{
-            json = { :post_id => @like.post_id,
-                                       :like_id => @like.id,
-                                       :html => render_to_string(
-                                         :partial => 'likes/like',
-                                         :locals => { :like => @like,
-                                           :person => current_user.person,
-                                          }
-                                        )
-                                      }
-            render(:json => json, :status => 201)
-          }
-          format.html{ render :nothing => true, :status => 201 }
+          respond_to do |format|
+            format.js{
+              json = { :post_id => @like.post_id,
+                                         :like_id => @like.id,
+                                         :html => render_to_string(
+                                           :partial => 'likes/like',
+                                           :locals => { :like => @like,
+                                             :person => current_user.person,
+                                           }
+                                          )
+                                        }
+              render(:json => json, :status => 201)
+            }
+            format.html{ render :nothing => true, :status => 201 }
+          end
+        else
+          render :nothing => true, :status => 406
         end
-      else
-        render :nothing => true, :status => 406
       end
     end
   end
-
 end
--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -40,10 +40,6 @@ describe LikesController do
         post :create, like_hash
         response.code.should == '201'
       end
-#      it "doesn't like twice" do
-#        post :create, like_hash
-#        response.code.should == '409'
-#      end
 #      it "doesn't like and dislike" do
 #        post :create, like_hash
 #        response.code.should == '409'
@@ -59,6 +55,11 @@ describe LikesController do
         like_hash[:id] = old_like.id
         post :create, like_hash
         old_like.reload.text.should == 'likes this'
+      end
+      it "doesn't like twice" do
+        post :create, like_hash
+        post :create, like_hash
+        response.code.should == '409'
       end
     end
     context 'on a post from a stranger' do
